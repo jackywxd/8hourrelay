dfba22ec991dc75f20c161cd898683dfc774ba79
Jacky Wu <jackywxd@gmail.com>
2023-06-19 12:53:18 -0700

    Add loading indicator and use it in message button

--- apps/web/src/app/message.tsx
+++ apps/web/src/app/message.tsx
@@ -3,132 +3,126 @@ import { useEffect, useState, useTransition } from "react";
 import { ErrorMessage, Form, Formik } from "formik";
 import * as Yup from "yup";
 import { sendMessage } from "./sendMsgActions";
+import Loading from "@/components/Loading";
+
 const init = {
-	name: "",
-	email: "",
-	messages: ""
+  name: "",
+  email: "",
+  messages: "",
 };
 function MessageForm() {
-	const [pending, startTransition] = useTransition();
-	const [initialValues, setInitialValues] = useState(init);
-	const [success, setSuccess] = useState(false);
-	const MessageSchema = Yup.object().shape({
-		name: Yup.string().max(50, "Too Long!").required("Required"),
-		email: Yup.string().email().required("Required"),
-		messages: Yup.string().required("Required")
-	});
+  const [pending, startTransition] = useTransition();
+  const [initialValues, setInitialValues] = useState(init);
+  const [success, setSuccess] = useState(false);
+  const MessageSchema = Yup.object().shape({
+    name: Yup.string().max(50, "Too Long!").required("Required"),
+    email: Yup.string().email().required("Required"),
+    messages: Yup.string().required("Required"),
+  });
 
-	useEffect(() => {
-		if (success && !pending) setInitialValues(init);
-	}, [success, pending]);
+  useEffect(() => {
+    if (success && !pending) setInitialValues(init);
+  }, [success, pending]);
 
-	const onSubmit = async (values, action) => {
-		if (pending || success) {
-			action.resetForm();
-			return;
-		}
-		startTransition(async () => {
-			await sendMessage(values);
-			setSuccess(true);
-			action.resetForm();
-		});
-	};
-	return (
-		<div className="content-container large text-white">
-			<div className="contact-deck">
-				Have question? Need some help?
-				<p className="description">
-					Send us messages and we will get back to you shortly.
-				</p>
-			</div>
+  const onSubmit = async (values, action) => {
+    if (pending || success) {
+      action.resetForm();
+      return;
+    }
+    startTransition(async () => {
+      await sendMessage(values);
+      setSuccess(true);
+      action.resetForm();
+    });
+  };
+  return (
+    <div className="content-container large text-white">
+      <div className="contact-deck">
+        Have question? Need some help?
+        <p className="description">
+          Send us messages and we will get back to you shortly.
+        </p>
+      </div>
 
-			<Formik
-				initialValues={initialValues}
-				validationSchema={MessageSchema}
-				enableReinitialize
-				onSubmit={onSubmit}>
-				{(props) => (
-					<Form>
-						<div className="form-container">
-							<div className="input-container">
-								<label htmlFor="first-name">
-									<span>Name</span>
-								</label>
-								<input
-									value={props.values.name}
-									onChange={props.handleChange}
-									type="text"
-									name="name"
-									id="first-name"
-								/>
-								<ErrorMessage name="name">
-									{(msg) => (
-										<p className="ml-2 text-sm text-red-600">
-											{msg}
-										</p>
-									)}
-								</ErrorMessage>
-							</div>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={MessageSchema}
+        enableReinitialize
+        onSubmit={onSubmit}
+      >
+        {(props) => (
+          <Form>
+            <div className="form-container">
+              <div className="input-container">
+                <label htmlFor="first-name">
+                  <span>Name</span>
+                </label>
+                <input
+                  value={props.values.name}
+                  onChange={props.handleChange}
+                  type="text"
+                  name="name"
+                  id="first-name"
+                />
+                <ErrorMessage name="name">
+                  {(msg) => <p className="ml-2 text-sm text-red-600">{msg}</p>}
+                </ErrorMessage>
+              </div>
 
-							<div className="input-container">
-								<label htmlFor="email">
-									<span>Email</span>
-								</label>
+              <div className="input-container">
+                <label htmlFor="email">
+                  <span>Email</span>
+                </label>
 
-								<input
-									value={props.values.email}
-									onChange={props.handleChange}
-									type="text"
-									name="email"
-									id="email"
-								/>
+                <input
+                  value={props.values.email}
+                  onChange={props.handleChange}
+                  type="text"
+                  name="email"
+                  id="email"
+                />
 
-								<ErrorMessage name="email">
-									{(msg) => (
-										<p className="ml-2 text-sm text-red-600">
-											{msg}
-										</p>
-									)}
-								</ErrorMessage>
-							</div>
+                <ErrorMessage name="email">
+                  {(msg) => <p className="ml-2 text-sm text-red-600">{msg}</p>}
+                </ErrorMessage>
+              </div>
 
-							<div className="input-container textarea span-2">
-								<label htmlFor="messages">
-									<span>Messages</span>
-								</label>
-								<textarea
-									value={props.values.messages}
-									onChange={props.handleChange}
-									id="messages"
-									name="messages"
-								/>
+              <div className="input-container textarea span-2">
+                <label htmlFor="messages">
+                  <span>Messages</span>
+                </label>
+                <textarea
+                  value={props.values.messages}
+                  onChange={props.handleChange}
+                  id="messages"
+                  name="messages"
+                />
 
-								<ErrorMessage name="messages">
-									{(msg) => (
-										<p className="ml-2 text-sm text-red-600">
-											{msg}
-										</p>
-									)}
-								</ErrorMessage>
-							</div>
-						</div>
+                <ErrorMessage name="messages">
+                  {(msg) => <p className="ml-2 text-sm text-red-600">{msg}</p>}
+                </ErrorMessage>
+              </div>
+            </div>
 
-						<div className="button-container mt-2 mb-20">
-							<button className="btn btn-large btn-primary blue">
-								{pending ? (
-									<span className="loading loading-ring loading-md"></span>
-								) : success ? (
-									`Sent successfully`
-								) : (
-									`Send`
-								)}
-							</button>
-						</div>
-					</Form>
-				)}
-			</Formik>
-		</div>
-	);
+            <div className="button-container mt-2 mb-20">
+              <button className="btn btn-large btn-primary blue w-full">
+                {pending ? (
+                  <div className="flex gap-1 items-center">
+                    <Loading />
+                    <span>Sending</span>
+                  </div>
+                ) : success ? (
+                  `Sent successfully`
+                ) : (
+                  `Send`
+                )}
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 }
 
 export default MessageForm;
