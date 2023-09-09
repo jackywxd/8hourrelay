907a933ff0f338120bc8cb8560602721898dddc1
Jacky Wu <jackywxd@gmail.com>
2023-09-09 11:07:31 -0700

    Remove console.log output

--- apps/web-admin/next.config.js
+++ apps/web-admin/next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
   experimental: {
     serverActions: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
